51f7a370e7d398300451da7479b3779321f72aff
zai <zai@txz.cool>
2021-03-29 11:30:18 +0800

    Update in 2021/3/29

--- Test/src/main/java/cool/txz/git/Test.java
+++ Test/src/main/java/cool/txz/git/Test.java
@@ -1,7 +1,11 @@
 package cool.txz.git;
 
+import cool.txz.git.init.Driver;
+import cool.txz.git.init.TestDriver;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Driver driver = new TestDriver();
+        driver.hello();
     }
 }
--- javaTest/src/main/java/Test.java
+++ javaTest/src/main/java/Test.java
@@ -1,6 +1,17 @@
+import sun.net.www.http.HttpClient;
+import thread.TestThread;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("HELLO");
+        TestThread testThread = new TestThread();
+        for (int i = 0; i < 100; i++) {
+            new Thread(testThread).start();
+        }
     }
 }
