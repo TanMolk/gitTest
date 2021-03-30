27a289e9ea91da662bed9add5d06145753e75a9e
zai <zai@txz.cool>
2021-03-30 15:25:13 +0800

    Update TestDriver.java for times

--- Test/src/main/java/cool/txz/git/init/TestDriver.java
+++ Test/src/main/java/cool/txz/git/init/TestDriver.java
@@ -6,5 +6,6 @@ public class TestDriver extends Driver{
     public void hello() {
         System.out.println("Hello Test");
         System.out.println("123");
+        System.out.println("345");
     }
 }
